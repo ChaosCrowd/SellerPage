a125c05d32841743a654958ff868d2308d359989
sjx <1104611441@qq.com>
2018-05-04 18:48:41 +0800

    test

---
+++ src/components/loginBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="loginBox">
+        <form action="" method="post">
+            <div class="container">
+                <div class="row">
+                    <input class="col-3" type="text" placeholder="Username" />
+                </div>
+                <div class="row">
+                    <input class="col-3" type="password" placeholder="Password" />
+                </div>
+                <div class="row">
+                    <input class="col-1" type="submit" />
+                    <router-link class="col-1">注册</router-link>
+                </div>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'loginBox'
+}
+</script>
+
+<style>
+input {
+    margin:5px 0;
+}
+
+</style>
